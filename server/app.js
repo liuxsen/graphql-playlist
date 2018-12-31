4d8e3d8677be190b8afca7527270872611887018
刘建辉 <jianhui@yzyw-inc.com>
2018-12-31 16:04:34 +0800

    save

--- server/app.js
+++ server/app.js
@@ -7,17 +7,20 @@ const app = express();
 
 // connect to mlab database
 // make sure to replace my db string & creds with your own
-mongoose.connect('mongodb://ninja:test@ds161148.mlab.com:61148/graphql-ninja')
+mongoose.connect('mongodb://127.0.0.1:27017');
 mongoose.connection.once('open', () => {
-    console.log('conneted to database');
+  console.log('conneted to database');
 });
 
 // bind express with graphql
-app.use('/graphql', graphqlHTTP({
+app.use(
+  '/graphql',
+  graphqlHTTP({
     schema,
     graphiql: true
-}));
+  })
+);
 
 app.listen(4000, () => {
-    console.log('now listening for requests on port 4000');
+  console.log('now listening for requests on port 4000');
 });
